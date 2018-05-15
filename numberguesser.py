7aa83733bdafcb37bce3a59d9a544e3958ba1b7f
jhassler1031 <jhassler1031@gmail.com>
2018-05-15 16:42:58 -0400

    Modified Getting Close

--- numberguesser.py
+++ numberguesser.py
@@ -12,6 +12,23 @@ while my_num < 0 or my_num > 100:
 counter = 1
 
 while my_num != computer_number:
+    if my_num - computer_number < 10 and computer_number - my_num < 10:
+        print("Getting close! ")
+    if my_num > computer_number:
+        my_num = input("Too high.  Guess again: ")
+        my_num = int(my_num)
+        counter += 1
+    else:
+        my_num = input("Too low.  Guess again: ")
+        my_num = int(my_num)
+        counter += 1
+
+
+
+#old version below - working
+
+"""
+while my_num != computer_number:
     if my_num > computer_number and my_num <= (computer_number + 10):
         my_num = input("Too high, but close.  Guess again: ")
         my_num = int(my_num)
@@ -28,7 +45,7 @@ while my_num != computer_number:
         my_num = input("Too low, guess again: ")
         my_num = int(my_num)
         counter += 1
-
+"""
 counter = str(counter)
 print("Congratulations, you guessed the number!")
 print("It took you " + counter + " times to guess the number.")

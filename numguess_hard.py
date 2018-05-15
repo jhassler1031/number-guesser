f7a5b8a0587691baa425947ddf3b75db1c07a8e3
jhassler1031 <jhassler1031@gmail.com>
2018-05-15 13:41:40 -0400

    numguess_hard.py

--- numguess_hard.py
+++ numguess_hard.py
@@ -12,8 +12,17 @@ computer_number = random.randint(0 , 101)
 counter = 1
 
 while computer_number != my_num:
-    
-
+    if computer_number > my_num and computer_number <= (my_num + 10):
+        computer_number = random.randint((computer_number - 10) , computer_number)
+        counter += 1
+    elif computer_number > my_num:
+        computer_number = random.randint(0 , computer_number)
+        counter += 1
+    elif computer_number < my_num and computer_number >= (my_num - 10):
+        computer_number = random.randint(computer_number , (computer_number + 10))
+        counter += 1
+    else:
+        computer_number = random.randint(0 , computer_number)
 
 counter = str(counter)
 print("The computer guessed your number in " + counter + " turns.")

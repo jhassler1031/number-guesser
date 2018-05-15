6ab610201127f7ce4e0f81c89c55ec82d7b366c2
jhassler1031 <jhassler1031@gmail.com>
2018-05-15 13:06:05 -0400

    numberguesser.py

--- numberguesser.py
+++ numberguesser.py
@@ -11,8 +11,12 @@ while my_num != computer_number:
     if my_num > computer_number:
         my_num = input("Too high, guess again: ")
         my_num = int(my_num)
+        counter += 1
     else:
         my_num = input("Too low, guess again: ")
         my_num = int(my_num)
+        counter += 1
 
+counter = str(counter)
 print("Congratulations, you guessed the number!")
+print("It took you " + counter + " times to guess the number.")

d405687e4846a792dabf1d736aa1692b7e2ae8b1
jhassler1031 <jhassler1031@gmail.com>
2018-05-15 14:14:36 -0400

    numguess_hard.py

--- numguess_hard.py
+++ numguess_hard.py
@@ -1,28 +1,30 @@
 
 import random
 
-human_num = input("Please enter a number between 0 and 100 for the computer to guess: ")
-human_num = int(human_num)
+my_num = input("Please enter a number between 0 and 100 for the computer to guess: ")
+my_num = int(my_num)
 
 while my_num < 0 or my_num > 100:
     my_num = input("That number is not between 0 and 100, please try again: ")
     my_num = int(my_num)
 
-computer_number = random.randint(0 , 101)
+#computer_number = random.randint(0 , 101)
+high_end = 100
+low_end = 0
+computer_number = (low_end + high_end) / 2
 counter = 1
 
 while computer_number != my_num:
-    if computer_number > my_num and computer_number <= (my_num + 10):
-        computer_number = random.randint((computer_number - 10) , computer_number)
-        counter += 1
-    elif computer_number > my_num:
-        computer_number = random.randint(0 , computer_number)
-        counter += 1
-    elif computer_number < my_num and computer_number >= (my_num - 10):
-        computer_number = random.randint(computer_number , (computer_number + 10))
+    if computer_number < my_num:
+        low_end = computer_number
+        computer_number = (low_end + high_end) / 2
+        computer_number = int(computer_number)
         counter += 1
     else:
-        computer_number = random.randint(0 , computer_number)
+        high_end = computer_number
+        computer_number = (low_end + high_end) / 2
+        computer_number = int(computer_number)
+        counter += 1
 
 counter = str(counter)
 print("The computer guessed your number in " + counter + " turns.")

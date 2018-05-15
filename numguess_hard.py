f9739e75a84c215b5d98d67ca95b33e7b8424af3
jhassler1031 <jhassler1031@gmail.com>
2018-05-15 13:32:55 -0400

    numguess_hard.py

--- numguess_hard.py
+++ numguess_hard.py
@@ -1,2 +1,19 @@
 
 import random
+
+human_num = input("Please enter a number between 0 and 100 for the computer to guess: ")
+human_num = int(human_num)
+
+while my_num < 0 or my_num > 100:
+    my_num = input("That number is not between 0 and 100, please try again: ")
+    my_num = int(my_num)
+
+computer_number = random.randint(0 , 101)
+counter = 1
+
+while computer_number != my_num:
+    
+
+
+counter = str(counter)
+print("The computer guessed your number in " + counter + " turns.")
